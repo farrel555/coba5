36e192a00acbe5ddb38b40346d6d42570e6781dc
farrel555 <azrelazmi2@gmail.com>
2025-05-26 20:22:13 +0700

    update2

--- webpack.common.js
+++ webpack.common.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/public/index.html'),
+      template: path.resolve(__dirname, './src/public/index.html'),
       inject: 'body',
     }),
   ],
